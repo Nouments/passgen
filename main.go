542894739ff4e641328dfe3b1add7c586b71024b
RAKOTONIRINA Manankasina Nomentsoa <100316615+Nouments@users.noreply.github.com>
2026-03-23 15:15:10 +0300

    update app to accept flag

--- main.go
+++ main.go
@@ -1,14 +1,10 @@
 package main
 
 import (
-	"bufio"
 	"crypto/rand"
+	"flag"
 	"fmt"
-	"log"
 	"math/big"
-	"os"
-	"strconv"
-	"strings"
 )
 
 func GeneratePassword(length int) (string, error) {
@@ -24,26 +20,10 @@ func GeneratePassword(length int) (string, error) {
 	return  string(password),nil
 }
 
-func ValidateInput(input string) (int,error){
-	val:= strings.TrimSpace(input)
-	intval,err:=strconv.Atoi(val)
-	if err!=nil{
-		return 0,fmt.Errorf("make sure input is integer")
-	}
-	return intval,nil
-}
 func main() {
-
-	reader:= bufio.NewReader(os.Stdin)
-	fmt.Println("Entered password lenth: ")
-	input,_:= reader.ReadString('\n')
-
-	length,err:=ValidateInput(input)
-	if err!=nil{
-		log.Fatal(err)
-	}
-
-	pwd, err:= GeneratePassword(length)
+	 length:=flag.Int("l",8,"length")
+	 flag.Parse()
+	pwd, err:= GeneratePassword(*length)
 	if err!=nil{
 		fmt.Println(err)
 		return 
